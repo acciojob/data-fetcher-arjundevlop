de6f09b7ad82ec128aafa3f1204ca54748eb8942
arjundevlop <arjun.tomar45@gmail.com>
2023-05-04 11:36:21 +0530

    submit1

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,29 @@
 
 import React from "react";
+import { useState,useEffect } from "react";
 import './../styles/App.css';
 
 const App = () => {
+
+  const [data, setdata] = useState([]);
+
+  useEffect(() => {
+     function fetchData() {
+      fetch("https://dummyjson.com/products")
+      .then((res)=>(res.json()))
+      .then((res)=>{setdata(res)})
+    }
+    fetchData();
+  }, []);
+
+  if (!data) {
+    return <p>Loading product data...</p>;
+  }
+
   return (
-    <div>
-        {/* Do not remove the main div */}
+    <div className="app">
+    <h1>Data Fetched from API</h1>
+        <pre>{JSON.stringify(data)}</pre>
     </div>
   )
 }
